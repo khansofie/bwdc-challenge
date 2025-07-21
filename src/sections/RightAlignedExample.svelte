6ef7d5061354519404a43b282725bdc402ff728c
Sofie Khan <sofiekhanorange@gmail.com>
2025-07-21 18:20:23 -0400

    changes

--- src/sections/RightAlignedExample.svelte
+++ src/sections/RightAlignedExample.svelte
@@ -4,11 +4,78 @@
     import { Chart } from "@highcharts/svelte";
     import Scroller from "../lib/Scroller.svelte";
     import ArticleText from "../lib/ArticleText.svelte";
-</script>
 
+    let options = {
+        chart: {
+            type: "pie",
+        },
+        title: {
+            text: "PIEEEEEEEEE",
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                dataLabels: [
+                    {
+                        enabled: true,
+                        distance: 20,
+                    },
+                    {
+                        enabled: true,
+                        distance: -40,
+                        format: "{point.percentage:.1f}%",
+                        style: {
+                            fontSize: "1.2em",
+                            textOutline: "none",
+                        },
+                        filter: {
+                            operator: ">",
+                            property: "percentage",
+                            value: 10,
+                        },
+                    },
+                ],
+            },
+        },
+        series: [
+            {
+                name: "Group",
+                data: [
+                    {
+                        name: "Group 1",
+                        y: 151,
+                    },
+                    {
+                        name: "Group 2",
+                        sliced: true,
+                        selected: true,
+                        y: 180,
+                    },
+                    {
+                        name: "Group 3",
+                        y: 32,
+                    },
+                    {
+                        name: "Group 4",
+                        y: 103,
+                    },
+                    {
+                        name: "Group 5",
+                        y: 77,
+                    },
+                ],
+            },
+        ],
+    };
+</script>
 
 <div>
     <Scroller layout="right">
+        {#snippet sticky()}
+            <div class="chart">
+                <Chart {options} Highcharts={Highcharts} />
+            </div>
+        {/snippet}
 
         {#snippet scrolly()}
             <ArticleText>
@@ -32,3 +99,13 @@
         {/snippet}
     </Scroller>
 </div>
+
+<style>
+    .chart {
+        width: 90%;
+        max-width: 600px;
+        margin: 0 auto;
+        display: block;
+        text-align: center;
+    }
+</style>
