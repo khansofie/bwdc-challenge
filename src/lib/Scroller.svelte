8b22831cc2af68f56a934d2babb5ed4c6f97a968
Sofie Khan <sofiekhanorange@gmail.com>
2025-07-21 23:59:02 -0400

    changes

--- src/lib/Scroller.svelte
+++ src/lib/Scroller.svelte
@@ -2,22 +2,22 @@
     // `layout` can be either "right" or "left"
     // `sticky` and `scrolly` are the snippets passed in (see one of the examples)
     let { layout, sticky, scrolly } = $props(); 
+    console.log("Scroller props:", { layout, sticky, scrolly });
 </script>
 
 <div class="wrapper {layout}">
     <div class="sticky">
-        {@render sticky()}
+        {@render sticky?.()}
     </div>
 
     <div class="scrolly">
-        {@render scrolly()}
+        {@render scrolly?.()}
     </div>
 </div>
 
 <style>
     .wrapper {
         background-color: #220d31;
-        /*padding: min(100vh, 30rem) 1rem;*/
         border-style: solid;
         border-color: #220d31;
         display: flex;
@@ -27,10 +27,11 @@
         min-height: 100vh;
         position: relative;
     }
-    /*Layout order*/
+
     .left .scrolly {
         order: 0;
     }
+
     .left .sticky {
         order: 1;
     }
@@ -38,15 +39,14 @@
     .right .scrolly {
         order: 1;
     }
+
     .right .sticky {
         order: 0;
     }
 
     .sticky,
     .scrolly {
-        /*display: flex;*/
-        /*flex-direction: column;*/
-        flex: 1 1 50%; /* Allows growing, shrinking */
+        flex: 1 1 50%;
     }
 
     .sticky {
@@ -56,23 +56,18 @@
         width: 100%;
         height: 100vh;
         z-index: 0;
-
         display: flex;
         align-items: center;
         justify-content: center;
-        
         background-color: #220d31;
         overflow: hidden;
         padding: 1.5rem;
-        box-shadow: 0 4px 12 px rgba(0,0,0,0.1);
+        box-shadow: 0 4px 12px rgba(0,0,0,0.1);
     }
 
     .scrolly {
         z-index: 1;
-        /*padding-left: 1.5rem;
-        padding-right: 1.5rem;*/
         padding: 2rem 1.5rem;
-        /*justify-content: flex-start;*/
         flex: 1 1 50%;
     }
 
@@ -82,20 +77,6 @@
             padding: 2rem 1rem;
             width: 100vw;
         }
-
-        /*.sticky,*/
-        /*.scrolly {*/
-            /*flex: 1 1 auto;*/
-            /*min-width: 100%;*/
-            /*position: static; remove sticky on mobile */
-            /*transform: none;/*
-        /*}*/
-
-        /*.sticky {*/
-            /*margin-bottom: 2rem;*/
-            /*min-height: auto;*/
-            /*background-color: #ffb6c1;*/
-        /*}*/
 
         .sticky {
             position: static;
